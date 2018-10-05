04fca6eda34d5a5963fd44248c32ddda76ee364c
Akshay. R <akhisreelibra@gmail.com>
2018-10-05 12:23:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+# print(type(iris))
+
+# print(iris.data)
+
+# print(iris.feature_names)
+
+# print(iris.target)
+
+# print(iris.target_names)
+
+# print(type(iris.data))
+# print(type(iris.target))
+
+# print(iris.data.shape)
+
+X = iris.data
+y = iris.target
+
+
+
+
